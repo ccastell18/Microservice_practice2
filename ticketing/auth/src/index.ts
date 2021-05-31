e8c04436a3537f0e65bc83cb83b327a35873bc1b
Chris Castellano <c.castellano18@gmail.com>
2021-05-31 00:03:55 -0500

    skaffold set up and working

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,6 +4,6 @@ import {json} from 'body-parser';
 const app = express()
 app.use(json())
 
-app.listen(3000, () =>
-console.log("listening on port 3000")
-)
+app.listen(3000, () =>{
+console.log("listening on port 3000!!!")
+})
