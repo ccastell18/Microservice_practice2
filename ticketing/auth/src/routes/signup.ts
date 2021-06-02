408c8be846e486a61f3b61e4c08710ef495cf6ff
Chris Castellano <c.castellano18@gmail.com>
2021-06-02 13:49:43 -0500

    errorHandler created

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -13,12 +13,14 @@ router.post('/api/users/signup',[
       .withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response)  =>{
   const errors = validationResult(req);
+  
   if(!errors.isEmpty()){
-    return res.status(400).send(errors.array())
+    throw new Error('Invalid email or password')
   }
   const {email, password} = req.body;
   
   console.log('Creating a user ...')
+  throw new Error('Error connecting to database')
 
   res.send({})
 
