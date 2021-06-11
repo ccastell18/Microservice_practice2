1ff62d814d621f35dbf4b9a1daebd600d12e6d69
Chris Castellano <c.castellano18@gmail.com>
2021-06-11 01:06:18 -0500

    UserDoc interface

--- ticketing/auth/src/models/users.ts
+++ ticketing/auth/src/models/users.ts
@@ -1,5 +1,22 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required to create a User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+//interface that describes the properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//interface that describes the properties a User Document has
+interface UserDoc extends mongoose.Document{
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +28,13 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema);
+//custom function built into a model. add statics to Schema
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+
 
 export {User}
