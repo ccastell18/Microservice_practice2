65cb4893ff1c294c7d9b51a6607774e4c7e44cc3
Chris Castellano <c.castellano18@gmail.com>
2021-06-12 00:03:18 -0500

    create user

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import { User } from '../models/users'
 
 
 import {RequestValidationError} from '../errors/request-validation-error'
-import {DatabaseConnectionError} from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -21,12 +21,19 @@ router.post('/api/users/signup',[
   if(!errors.isEmpty()){
     throw new RequestValidationError(errors.array())
   }
-  
-  
-  console.log('Creating a user ...')
-  throw new DatabaseConnectionError()
 
-  res.send({})
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({email});
+  if(existingUser){
+    console.log('Email in use');
+    return res.send({})
+  }
+
+  const user = User.build({email, password})
+  await user.save();
+
+  res.status(201).send(user)
 
 })
 
