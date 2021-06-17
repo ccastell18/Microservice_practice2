3d8662ca4a83d334b680b489a01623fb38897ec4
Chris Castellano <c.castellano18@gmail.com>
2021-06-17 13:36:31 -0500

    current user route works

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { isTypeQueryNode } from 'typescript';
+
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res)  =>{
-  console.log('Hello World')
+  //question mark checks to see if an internal property exists or not.
+  if(!req.session?.jwt){
+    return res.send({current: null})
+  }  
+  try{
+  //verify will throw an error
+  const payload = jwt.verify(
+    req.session.jwt, 
+    process.env.JWT_KEY!
+    );
+    res.send({currentUser: payload})
+  }catch(err){
+    res.send({currentUser: null})
+  }
 })
 
 export {router as currentUserRouter}
