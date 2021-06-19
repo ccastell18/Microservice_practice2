62d08d8b88d453c36da0d2d89d481e44aeae4186
Chris Castellano <c.castellano18@gmail.com>
2021-06-18 23:12:50 -0500

    signin test successful

---
+++ ticketing/auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,51 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it("fails when an email does not exist is supplied", async () =>{
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(400)
+})
+
+it('fails when an incorrect password is supplied', async () =>{
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: "test@test.com",
+      password: "a;lsdf"
+    })
+    .expect(400)
+})
+
+it('it responds with a cookie when sucessfully signed in', async () =>{
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: "test@test.com",
+      password: "password"
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+})
+
